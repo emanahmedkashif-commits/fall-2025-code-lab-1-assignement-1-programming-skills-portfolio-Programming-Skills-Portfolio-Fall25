ef757a5394cefaf69655c414f92f4efc0aebd7d3
emanahmedkashif-commits <emanahmedkashif@gmail.com>
2025-11-20 12:46:25 +0400

    Update Task_2.py

--- Exercise 02- Simple Sums/Task_2.py
+++ Exercise 02- Simple Sums/Task_2.py
@@ -1,4 +1,4 @@
-number1= 8   # A variable has stored the digit "8".
-number2= 10   # A variable has stored another digit "10".
+number1= 8                  # A variable has stored the digit "8".
+number2= 10                 # A variable has stored another digit "10".
 sum = (number1 + number2)   # A variable "sum" has been created. This variable adds the two previously created numerical variables together.
-print (sum) # Displays the num of the two digits.
+print (sum)                 # Displays the num of the two digits.
