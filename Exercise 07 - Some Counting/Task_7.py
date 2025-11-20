cbb4e883103955ba303d72a66f9a77e513f85c4b
emanahmedkashif-commits <emanahmedkashif@gmail.com>
2025-11-20 13:04:54 +0400

    Have utilized for loops, if/else statements, functions to complete the tasks..

---
+++ Exercise 07 - Some Counting/Task_7.py
@@ -0,0 +1,19 @@
+print ("From 0 to 50")                           # Creates a title for the numbers to be displayed.
+for x in range (0 , 51 , 1):
+    print (x)                                    # Displays the digits according to the range mentioned.
+
+print ("\n From 50 to 0")                        # Enters a new line, then creates a title for the numbers to be displayed.
+for x in range (50 , -1, -1):
+    print (x)                                    # Displays the digits according to the range mentioned.
+
+print ("\n From 30 to 50")                       # Enters a new line, then creates a title for the numbers to be displayed.
+for x in range (30 , 51 , 1):
+    print (x)                                    # Displays the digits according to the range mentioned.
+
+print ("\n From 50 to 10 in intervals of 2")     # Enters a new line, then creates a title for the numbers to be displayed.
+for x in range (50 , 8 , -2):
+    print (x)                                    # Displays the digits according to the range mentioned.
+
+print ("\n From 100 to 200 in intervals of 5")   # Enters a new line, then creates a title for the numbers to be displayed.
+for x in range (100 , 201 , +5):
+    print (x)                                    # Displays the digits according to the range mentioned.
