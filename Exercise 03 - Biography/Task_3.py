037a8d487bd643c3b968e8e09ba6beb4b28c1be0
emanahmedkashif-commits <emanahmedkashif@gmail.com>
2025-11-20 12:48:52 +0400

    Update Task_3.py

--- Exercise 03 - Biography/Task_3.py
+++ Exercise 03 - Biography/Task_3.py
@@ -1,18 +1,21 @@
-name = input("Enter your full name: ")   # Created a variable "name".
-hometown = input("Enter your hometown: ")   # Created a variable "hometown".
-age = int(input("Enter your age: "))   # Created a variable "age".
+name = input("Enter your full name: ")           # Created a variable "name".
+hometown = input("Enter your hometown: ")        # Created a variable "hometown".
 
-information = {   # Created a dictionary to store this information.
+while True:                                      # Continue the statement if true.
+        age_ask = input("Enter your age: ")      # Created a variable "age".
+
+        if age_ask.isdigit():                    # Continue if the age is a digit.
+         age = int(age_ask)
+         break
+        else:
+          print ("Please type a numerical.")     # Display if a sring has been typed.
+        age = None
+
+information = {                                  # Created a dictionary to store this information.
     "name" : name,
     "hometown" : hometown,
     "age" : age
 }
 
-
-print (f"{information["name"]}\n{information["hometown"]}\n{information["age"]}")   # Displays this information.
-
-
-
-
-
-
+if age is not None:
+    print (f"{information["name"]}\n{information["hometown"]}\n{information["age"]}")   # Displays this information.
